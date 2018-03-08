555884a2fab1781e406f09d9a86611874bd4bb19
Dmitrijs Minajevs <d3x133@gmail.com>
2018-03-08 18:58:44 +0200

    Update HRPG Rift sound notification.user.js

--- HRPG Rift sound notification.user.js
+++ HRPG Rift sound notification.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HRPG Rift sound notification
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://www.heroesrpg.com/*
@@ -10,7 +10,7 @@
 
 (function() {
     'use strict';
-    $('#header').prepend('<a>Rift notifier by Code v01</a> | ');
+    $('#header').prepend('<a>Rift notifier by Code v02</a> | ');
 
     var _audio;
     $( document ).ajaxSuccess(function( event, xhr, settings ) {
@@ -25,7 +25,8 @@
             $.each(data.c, function(i, item) {
                 if($.inArray(data.c[i].uid, ignored) < 0) {
                     if(data.c[i].type == 10) {
-                        if(data.c[i].message.indexOf('A Rift will open in 5 mintues') !== -1){
+                        console.log(data.c[i].message.indexOf('A Rift will open in 5 minutes') !== -1);
+                        if(data.c[i].message.indexOf('A Rift will open in 5 minutes') !== -1){
                             _play();
                         }
                     }
@@ -41,4 +42,3 @@
         _audio.play();
     }
 })();
-
